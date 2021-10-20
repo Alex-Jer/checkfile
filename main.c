89814f417e8501fd8f6664a110c516231051a7c9
Alex-Jer <alexandrejeronimo2001@gmail.com>
2021-10-20 19:14:21 +0100

    jpeg fix

--- main.c
+++ main.c
@@ -16,8 +16,6 @@
 
 #define MAX 128
 
-int validateFileType(char *extension, char *mimeType);
-
 //? Tem que ser global?
 char *filepath = NULL;
 
@@ -34,54 +32,52 @@ int main(int argc, char *argv[]) {
   char *trueExtension, *extension = strrchr(filepath, '.') + 1, *filename = strrchr(filepath, '/') + 1;
 
   // TODO: Concluir mensagem de erro
-  if (argc < 2)
-    ERROR(1, "[ERROR] must have at least one argument! usage: ...\n");
+  if (argc < 2) {
+    fprintf(stderr, "[ERROR] must have at least one argument! usage: ...\n");
+    exit(1);
+  }
 
+  // Check if the given file exists
   if (access(filepath, F_OK) != 0) {
     fprintf(stderr, "[ERROR] cannot open file '%s' -- No such file or directory\n", filepath);
+    exit(1);
+  }
+
+  if (strcmp(extension, "pdf") && strcmp(extension, "gif") && strcmp(extension, "jpg") && strcmp(extension, "png") &&
+      strcmp(extension, "mp4") && strcmp(extension, "zip") && strcmp(extension, "html")) {
+    fprintf(stderr, "Invalid\n");  // TODO: Concluir mensagem de erro
     exit(1);
   }
 
   pid_t pid;
   switch (pid = fork()) {
     case -1: /* Error */
-      ERROR(1, "fork() failed\n");
-      break;  //* Break desnecessário
+      ERROR(1, "fork() failed!\n");
+      break;  //* Break desnecessário?
     case 0:   /* Child */
       fd = open("tmp_output", O_WRONLY | O_CREAT, 0666);
       dup2(fd, 1);  // Opens the file descriptor on stdout (descriptor 1)
       close(fd);
       execl("/bin/file", "file", "--mime-type", "-b", filepath, NULL);
-      ERROR(1, "execlp() failed\n");
-      break;      //* Break desnecessário
+      ERROR(1, "execl() failed!\n");
+      break;      //* Break desnecessário?
     default:      /* Parent */
       wait(NULL); /* Waits for the child process to end */
       fptr = fopen("tmp_output", "r");
-      if (!fptr)
-        ERROR(1, "Could not open tmp_file!\n");
       fgets(mimeType, MAX, fptr);
       mimeType[strlen(mimeType) - 1] = '\0';
       trueExtension = strchr(mimeType, '/') + 1;
 
       if (!strcmp(extension, trueExtension)) {
         printf("[OK] '%s': extension '%s' matches file type '%s'\n", filename, extension, trueExtension);
+      } else if (strcmp(extension, trueExtension) && !strcmp(trueExtension, "jpeg") && !strcmp(extension, "jpg")) {
+        printf("[OK] '%s': extension '%s' matches file type '%s'\n", filename, extension, trueExtension);
       } else {
         printf("[MISMATCH] '%s': extension is '%s', file type is '%s'\n", filename, extension, trueExtension);
       }
 
-      //! Debug
-      // printf("%s\n", mimeType);
-      //! Debug
-
       fclose(fptr);
       remove("tmp_output");
-      break;
   }
   return 0;
 }
-
-// int validateFileType(char *extension, char *mimeType) {
-//   if (strcmp(extension, mimeType) == 0)
-//     return 1;
-//   return 0;
-// }
